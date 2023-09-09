72298c2f388ae9e1718fa7faa4d466482af1ef92
Jeffreysjb <114588695+Jeffreysjb@users.noreply.github.com>
2023-09-09 12:46:15 +0100

    Update 2 index.js
    Update 2

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const currentDate = new Date();
-const currentDay = daysOfWeek[currentDate.getUTCDay()];
-const currentUTCTime = currentDate.getTime();
+function updateTime() {
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDate = new Date();
+    const currentDay = daysOfWeek[currentDate.getUTCDay()];
+    const currentUTCTimeMilliseconds = currentDate.getTime();
 
-document.querySelector('.day').textContent = `${currentDay}`;
-document.querySelector('.time').textContent = `${currentUTCTime}`;
+    document.querySelector('.day').textContent = `${currentDay}`;
+    document.querySelector('.time').textContent = `${currentUTCTimeMilliseconds} ms`;
+}
+updateTime();
+
+setInterval(updateTime, 1000);
